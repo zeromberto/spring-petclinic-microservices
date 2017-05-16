f170d903b8f80da708ff7055777c47db1aaecbab
mrombach <matthias.rombach@itk-engineering.de>
2017-05-16 17:24:44 +0200

    removed caching in vets-service

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -1,22 +1,18 @@
 package org.springframework.samples.petclinic.vets;
 
+import kieker.monitoring.probe.spring.executions.OperationExecutionWebRequestRegistrationInterceptor;
+import kieker.monitoring.probe.spring.flow.RestInInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.samples.petclinic.vets.infrastructure.config.VetsProperties;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import kieker.monitoring.probe.spring.executions.OperationExecutionWebRequestRegistrationInterceptor;
-import kieker.monitoring.probe.spring.flow.RestInInterceptor;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @Configuration
-@EnableConfigurationProperties(VetsProperties.class)
 @ImportResource("classpath:/META-INF/aop.xml")
 public class VetsServiceApplication extends WebMvcConfigurerAdapter {
 
